97c5f6bac098dec9831befb4bb4b9b8482c5f424
ToiMaiTaiMoi_Tum <iloveusomuchx2@gmail.com>
2024-11-29 16:15:23 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int y;
+    int even = 0; 
+    int odd = 0;  
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> y;
+
+    while (y != 0) {
+        if (y % 2 == 0) {
+            even = even + 1; 
+        } else {
+            odd = odd + 1;   
+        }
+        cout << "Enter an integer: ";
+        cin >> y;
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
+
     return 0;
 }
